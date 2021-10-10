49c576e6e3dd5ba6d6932e23bec582825e8718f9
decoder <decoder@live.de>
2021-10-10 15:11:53 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# How to create Kubernetes YAML - playground
+
+This project includes a devcontainer setup with following tools installed (latest versions):
+- kubernetes
+- kubectl
+- helm
+- docker
+- docker-compose
+- yq
+- cdk8s
+- naml
+- Go
+- TypeScript
+
+## How to use
+
+With the tools pre-installed you can experiment with different ways to generate Kubernetes YAML.
---
+++ cdk8s/main.ts
@@ -0,0 +1,47 @@
+import { Construct } from 'constructs';
+import { App, Chart, ChartProps } from 'cdk8s';
+
+// imported constructs
+import { KubeDeployment, KubeService, IntOrString } from './imports/k8s';
+
+
+export class MyChart extends Chart {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
+    super(scope, id, props);
+
+    const label = { app: 'hello-k8s' };
+
+    new KubeService(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [{ port: 80, targetPort: IntOrString.fromNumber(8080) }],
+        selector: label
+      }
+    });
+
+    new KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [{ containerPort: 8080 }]
+              }
+            ]
+          }
+        }
+      }
+    });
+  }
+}
+
+const app = new App();
+new MyChart(app, 'hello');
+app.synth();
